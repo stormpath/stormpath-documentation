d83147ecdaf54931f48159cf0b9f81a1a9660232
Brian Retterer <brian@stormpath.com>
2016-03-17 14:21:13 -0400

    create cloud directory

---
+++ source/code/php/account_management/create_cloud_dir_req.php
@@ -0,0 +1,10 @@
+$tenant = $client->getCurrentTenant();
+
+$directory = $client->
+                dataStore->
+                instantiate(\Stormpath\Resource\Directory::class);
+
+$directory->name = 'Galactic Republic';
+$directory->description = 'Keeping Peace Across The Galaxy';
+
+$directory = $tenant->createDirectory($directory);
---
+++ source/code/php/account_management/create_cloud_dir_resp.php
@@ -0,0 +1,40 @@
+object(Stormpath\Resource\Resource)
+    public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz'
+    public 'name' => string 'Galactic Republic'
+    public 'description' => string 'Keeping Peace Across The Galaxy'
+    public 'status' => string 'ENABLED'
+    public 'createdAt' => string '2016-03-17T18:16:14.408Z'
+    public 'modifiedAt' => string '2016-03-17T18:16:14.408Z'
+    public 'tenant' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/tenants/6g1ZmmHTnCNWgdJqCD46ZO'
+    public 'provider' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/provider'
+    public 'customData' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/customData'
+    public 'passwordPolicy' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/passwordPolicies/2bofMIN0rJlxgefvFYloQz'
+    public 'accountCreationPolicy' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/accountCreationPolicies/2bofMIN0rJlxgefvFYloQz'
+    public 'accounts' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/accounts'
+    public 'applicationMappings' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/applicationMappings'
+    public 'applications' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/applications'
+    public 'groups' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/groups'
+    public 'organizations' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/organizations'
+    public 'organizationMappings' =>
+        object(stdClass)
+            public 'href' => string 'https://api.stormpath.com/v1/directories/2bofMIN0rJlxgefvFYloQz/organizationMappings'
