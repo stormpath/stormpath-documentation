9f9e48bbfa381ffec8a223cb5eeb002ff4f43d25
nbarbettini <nathanaelb@gmail.com>
2016-06-08 14:27:58 -0700

    Fix indentation

--- source/code/vbnet/idsite/idsite_reset_pwd.vb
+++ source/code/vbnet/idsite/idsite_reset_pwd.vb
@@ -1,5 +1,5 @@
 Dim logoutUrl = app.NewIdSiteUrlBuilder() _
-              .SetCallbackUri("http://mysite.foo/idsiteCallback") _
-              .SetPath("/#/reset") _
-              .SetSpToken(sptoken_from_url) _
-              .Build()
+    .SetCallbackUri("http://mysite.foo/idsiteCallback") _
+    .SetPath("/#/reset") _
+    .SetSpToken(sptoken_from_url) _
+    .Build()
