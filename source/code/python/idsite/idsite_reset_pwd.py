71c9086f60cc0984797ceb920cde764521b787d0
Randall Degges <r@rdegges.com>
2016-07-26 14:03:14 -0700

    Finishing Python ID Site docs.

---
+++ source/code/python/idsite/idsite_reset_pwd.py
@@ -0,0 +1,2 @@
+url = application.build_id_site_redirect_url('https://mysite.foo/callbacks/id_site', path='/#/reset')
+# Redirect the user to url.
---
+++ source/code/python/idsite/logout_from_idsite_req.py
@@ -0,0 +1,2 @@
+url = application.build_id_site_redirect_url('https://mysite.foo/callbacks/id_site', logout=True)
+# Redirect the user to url.
