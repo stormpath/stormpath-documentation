892c9b7c881079a0836a253cf7fce7f8529f8246
Julio Arias <jarias@users.noreply.github.com>
2016-08-09 12:08:35 -0600

    Added custom data search Java code sample
    Fixes stormpath/stormpath-sdk-java#509
    Related to #78

---
+++ source/code/java/account_management/cd_search.java
@@ -0,0 +1,5 @@
+//Currently there is no specific API methods to do search in the Java SDK
+//still you can leverege regular query params to use custom data search.
+Map<String, Object> queryParams = new HashMap<String, Object>();
+queryParams.put("customData.startDate", "[2012,2015]");
+AccountList accounts = directory.getAccounts(queryParams);
